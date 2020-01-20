48fe0b59ed61a2a337803fa19135846f250c9489
prolinebot <59023477+prolinebot@users.noreply.github.com>
2020-01-20 22:19:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -116,3 +116,120 @@ const port = config.port;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+const functions = require('firebase-functions')
+const request = require('request-promise')
+
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message'
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer {
+const functions = require('firebase-functions')
+const request = require('request-promise')
+
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message'
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer {Authen Key Line}` //// ----> LINE Authentication Key (Channel access token)
+}
+
+let node_ssh = require('node-ssh')
+let ssh = new node_ssh()
+
+exports.LineBot = functions.https.onRequest((req, res) => {
+  reply(req.body)
+})
+
+const reply = bodyResponse => {
+  const command = bodyResponse.events[0].message.text
+  ssh
+    .connect({
+      host: 'ec2.compute.amazonaws.com', //// ----> host ของเครื่องที่เราจะเข้าไปใช้งาน
+      username: 'ec2-user',
+      privateKey: 'privatekey.pem'  //// ----> ที่อยู่ของ privatekey.pem ไว้ที่ /functions/privatekey.pem
+    })
+    .then((result, error) => {
+      ssh
+        .execCommand(command, { cwd: '/var/www' })
+        .then(function(result) {
+          request({
+            method: `POST`,
+            uri: `${LINE_MESSAGING_API}/reply`,
+            headers: LINE_HEADER,
+            body: JSON.stringify({
+              replyToken: bodyResponse.events[0].replyToken,
+              messages: [
+                {
+                  type: `text`,
+                  text: result.stdout
+                }
+              ]
+            })
+          })
+            .then(() => {
+              return res.status(200).send(`Done`)
+            })
+            .catch(error => {
+              return Promise.reject(error)
+            })
+        })
+        .catch(error => {
+          return Promise.reject(error)
+        })
+    })
+    .catch(error => {
+      return Promise.reject(error)
+    })
+}
+view rawsshlinebot.js hosted with ❤ by GitHub}` //// ----> LINE Authentication Key (Channel access token)
+}
+
+let node_ssh = require('node-ssh')
+let ssh = new node_ssh()
+
+exports.LineBot = functions.https.onRequest((req, res) => {
+  reply(req.body)
+})
+
+const reply = bodyResponse => {
+  const command = bodyResponse.events[0].message.text
+  ssh
+    .connect({
+      host: 'ec2.compute.amazonaws.com', //// ----> host ของเครื่องที่เราจะเข้าไปใช้งาน
+      username: 'ec2-user',
+      privateKey: 'privatekey.pem'  //// ----> ที่อยู่ของ privatekey.pem ไว้ที่ /functions/privatekey.pem
+    })
+    .then((result, error) => {
+      ssh
+        .execCommand(command, { cwd: '/var/www' })
+        .then(function(result) {
+          request({
+            method: `POST`,
+            uri: `${LINE_MESSAGING_API}/reply`,
+            headers: LINE_HEADER,
+            body: JSON.stringify({
+              replyToken: bodyResponse.events[0].replyToken,
+              messages: [
+                {
+                  type: `text`,
+                  text: result.stdout
+                }
+              ]
+            })
+          })
+            .then(() => {
+              return res.status(200).send(`Done`)
+            })
+            .catch(error => {
+              return Promise.reject(error)
+            })
+        })
+        .catch(error => {
+          return Promise.reject(error)
+        })
+    })
+    .catch(error => {
+      return Promise.reject(error)
+    })
+}
+view rawsshlinebot.js hosted with ❤ by GitHub
